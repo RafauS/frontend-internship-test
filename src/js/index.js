c73d7f949ac4bc50d36b559898705429f0cd492d
RafauS <volfen27@gmail.com>
2020-06-07 16:43:03 +0200

    task 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
-/* Here goes your JS code */
+const mainBtn = document.querySelector(".btn-main")
+const mainContainer = document.querySelector(".main")
+const popupContainer = document.querySelector(".popup")
+const closeBtn = document.querySelector(".close-btn")
+
+mainBtn.addEventListener('click', changeContainer)
+closeBtn.addEventListener('click', changeContainer)
+
+
+
+function changeContainer(){
+    mainContainer.classList.toggle('hidden')
+    popupContainer.classList.toggle('hidden')
+}
